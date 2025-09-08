69ff239263c2c1ddc3f1659debd01622c9690524
katsilv90 <katherinesilv@gmail.com>
2025-09-08 12:01:07 +0100

    aula 1 java

---
+++ src/MinhaInfo.java
@@ -0,0 +1,7 @@
+public class MinhaInfo {
+    public static void main(String[] args) {
+        System.out.println("Nome: Katherine Silvestre");
+        System.out.println("Idade: 35");
+        System.out.println("Curso: Programação de Informática");
+    }
+}
---
+++ src/Soma.java
@@ -0,0 +1,9 @@
+public class Soma {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 6;
+        int soma = a+b;
+
+        System.out.println("soma=" + soma);
+    }
+}
